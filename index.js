0d7d618994879de3368bce9eea00e2219ba16379
Alex Milroy <amilroy@gmail.com>
2020-09-30 21:25:00 -0700

    moving in a new direction, asking questions in functions to allow for saving to an array

--- index.js
+++ index.js
@@ -1,81 +1,125 @@
-//npm init
-// install dependencies - inquirer
-
 //TODO: require all the dependencies I'll need - inquirer, fs
 var fs = require("fs")
 var inquirer = require("inquirer");
+//npm init
+// install dependencies - inquirer
+
+//==============================================================================
+//Global Variables
+//==============================================================================
+let readmeContent
+
+//==============================================================================
+//Application initilization
+//==============================================================================
+
+
 //TODO: array of questions for user
-const questions = [
+// const questions = {
+    inquirer.prompt([
     //TODO: Title question
-    Title: .prompt([
-        {
-          type: "input",
-          message: "What is your user name?",
-          name: "username"
-        }
+    {
+        type: "input",
+        message: "What would you like the title of your document to be?",
+        name: "title",
+    },
 
     //TODO: Description question
-
+    {
+        type: "input",
+        message: "Please provide the description for your readme file.",
+        name: "readmeDescription",
+    },
     //TODO: Table of Contents Question
-
+    {
+        type: "input",
+        message: "Please provide the table of contents text.",
+        name: "tableOfContents",
+    },
     //TODO: Installation Question
-
+    {
+        type: "input",
+        message: "Please provide the installation instructions for your application?",
+        name: "installation",
+    },
     //TODO: Usage Question
-
+    {
+        type: "input",
+        message: "Please provide a description of how to use your application",
+        name: "howToUse",
+    },
     //TODO: License question
-
+    {
+        type: "list",
+        message: "What type of license do you wish to utilize with your application",
+        name: "license",
+        choices: [
+            "MIT License",
+            "GNU GPLv3",
+        ]
+    },
     //TODO: Contributing question
-
+    {
+        type: "list",
+        message: "Would you like to allow other developers to contribute to your application?",
+        name: "contribute",
+        choices: [
+            "Yes",
+            "No",
+        ]
+    },
     //TODO: Tests question
+    {
+        type: "input",
+        message: "What tests can your users run to ensure your application is running correctly?",
+        name: "howToUse",
+    },
 
     //TODO: Questions
-];
-
-//writing a readme in a markdown file
-
-//TODO: Function that will generate the readme template
-
-// function writeToFile(fileName, data) {
-// }
-
-//TODO: use inquirer to prompt users with questions
-
-//TODO: Use answers that come back from inquirer - pass those into the generate readme function
-
-//TODO: Write file using template generated from readme function
-
-// function to initialize program
-// function init() {
-
-// }
-
-// function call to initialize program
-// init();
-
-inquirer
-  .prompt([
     {
-      type: "input",
-      message: "What is your user name?",
-      name: "username"
-    },
-    {
-      type: "password",
-      message: "What is your password?",
-      name: "password"
-    },
-    {
-      type: "password",
-      message: "Re-enter password to confirm:",
-      name: "confirm"
+        type: "checkbox",
+        message: "Which methods can users use to contact you with questions?",
+        name: "questions",
+        choices: [
+            "email",
+            "phone",
+            "github",
+            "linkedin",
+            "postal mail",
+        ]
     }
-  ])
-  .then(function(response) {
+    // }
 
-    if (response.confirm === response.password) {
+    //writing a readme in a markdown file
+]).then(function(data) {
+
+    var filename = data.title.toLowerCase().split(' ').join('') + ".md";
+  
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+  
+      if (err) {
+        return console.log(err);
+      }
+  
       console.log("Success!");
-    }
-    else {
-      console.log("You forgot your password already?!");
-    }
+  
+    });
   });
+    //TODO: Function that will generate the readme template
+
+    // function writeToFile(fileName, data) {
+    // }
+
+    //TODO: use inquirer to prompt users with questions
+
+    //TODO: Use answers that come back from inquirer - pass those into the generate readme function
+
+    //TODO: Write file using template generated from readme function
+
+    // function to initialize program
+    // function init() {
+
+    // }
+
+    // function call to initialize program
+    // init();
