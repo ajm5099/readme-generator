e572dc6c040e47368db7b5365c90ca7f39c0cc20
Alex Milroy <amilroy@gmail.com>
2020-09-30 22:32:08 -0700

    have all questions working and answers in an array

--- index.js
+++ index.js
@@ -7,16 +7,195 @@ var inquirer = require("inquirer");
 //==============================================================================
 //Global Variables
 //==============================================================================
-let readmeContent
+let content = [];
 
 //==============================================================================
 //Application initilization
 //==============================================================================
 
+const startApp = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to create a new readme file?",
+                name: "start",
+                choices: ["Create new readme", "Exit"]
+            },
+        ])
+        .then((res) => {
+            if (res.start === "exit") {
+                return;
+            } else {
+                getTitle();
+            }
+        });
+};
 
-//TODO: array of questions for user
-// const questions = {
-    inquirer.prompt([
+//==============================================================================
+//Questions and user input
+//==============================================================================
+
+//get the title of the document
+const getTitle = () => {
+    inquirer
+        .prompt([
+            {
+                name: "readme_title",
+                type: "input",
+                message: "What would you like the title of your document to be?",
+            },
+        ])
+        .then((res) => {
+            content.title = res.readme_title;
+            getDescription();
+        });
+};
+
+//get the description of the document
+const getDescription = () => {
+    inquirer
+        .prompt([
+            {
+                name: "readme_description",
+                type: "input",
+                message: "Please provide the description for your readme file.",
+            },
+        ])
+        .then((res) => {
+            content.description = res.readme_description;
+            getTableContent();
+        });
+};
+
+//get the table of contents
+const getTableContent = () => {
+    inquirer
+        .prompt([
+            {
+                name: "tableOfContents",
+                type: "input",
+                message: "Please provide the table of contents text.",
+            },
+        ])
+        .then((res) => {
+            content.toc = res.tableOfContents;
+            getInstallQuestions();
+        });
+};
+
+const getInstallQuestions = () => {
+    inquirer
+        .prompt([
+            {
+                name: "installation",
+                type: "input",
+                message: "Please provide the installation instructions for your application?",
+            },
+        ])
+        .then((res) => {
+            content.installation = res.installation;
+            getUsage();
+        });
+};
+
+const getUsage = () => {
+    inquirer
+        .prompt([
+            {
+                name: "howToUse",
+                type: "input",
+                message: "Please provide a description of how to use your application",
+            },
+        ])
+        .then((res) => {
+            content.usage = res.howToUse;
+            getLicense();
+        });
+};
+
+const getLicense = () => {
+    inquirer
+        .prompt([
+            {
+                name: "license",
+                type: "list",
+                message: "What type of license do you wish to utilize with your application",
+                choices: [
+                    "MIT License",
+                    "GNU GPLv3",
+                ]
+            },
+        ])
+        .then((res) => {
+            content.license = res.license;
+            getContributing();
+        });
+};
+
+const getContributing = () => {
+    inquirer
+        .prompt([
+            {
+                name: "contribute",
+                type: "list",
+                message: "Would you like to allow other developers to contribute to your application?",
+                choices: [
+                    "Yes",
+                    "No",
+                ]
+            },
+        ])
+        .then((res) => {
+            content.contributing = res.contribute;
+            getTests();
+        });
+};
+
+const getTests = () => {
+    inquirer
+        .prompt([
+            {
+                name: "howToUse",
+                type: "input",
+                message: "What tests can your users run to ensure your application is running correctly?",
+            },
+        ])
+        .then((res) => {
+            content.tests = res.howToUse;
+            getQuestions();
+        });
+};
+
+const getQuestions = () => {
+    inquirer
+        .prompt([
+            {
+                name: "questions",
+                type: "checkbox",
+                message: "Which methods can users use to contact you with questions?",
+                choices: [
+                    "email",
+                    "phone",
+                    "github",
+                    "linkedin",
+                    "postal mail",
+                ]
+            },
+        ])
+        .then((res) => {
+            content.questions = res.questions;
+            console.log(content);
+        });
+};
+startApp();
+
+
+
+
+
+/*
+inquirer.prompt([
     //TODO: Title question
     {
         type: "input",
@@ -88,23 +267,25 @@ let readmeContent
             "postal mail",
         ]
     }
-    // }
 
     //writing a readme in a markdown file
-]).then(function(data) {
+]).then(function (data) {
 
     var filename = data.title.toLowerCase().split(' ').join('') + ".md";
-  
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-  
-      if (err) {
-        return console.log(err);
-      }
-  
-      console.log("Success!");
-  
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+
     });
-  });
+});
+*/
+
+
     //TODO: Function that will generate the readme template
 
     // function writeToFile(fileName, data) {
