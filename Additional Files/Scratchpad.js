bbaffb6afb52c09de94d080b1b865ff1067461e6
Alex Milroy <amilroy@gmail.com>
2020-10-02 17:34:29 -0700

    final touches, finished creating project readme

---
+++ Additional Files/Scratchpad.js
@@ -0,0 +1,265 @@
+const startApp = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to create a new readme file?",
+                name: "start",
+                choices: ["Create new readme", "Exit"]
+            },
+        ])
+        .then((res) => {
+            if (res.start === "exit") {
+                return;
+            } else {
+                getTitle();
+            }
+        });
+};
+
+//get the title of the document
+const getTitle = () => {
+    inquirer
+        .prompt([
+            {
+                name: "readme_title",
+                type: "input",
+                message: "What would you like the title of your document to be?",
+            },
+        ])
+        .then((res) => {
+            content.title = res.readme_title;
+            getDescription();
+        });
+};
+
+//get the description of the document
+const getDescription = () => {
+    inquirer
+        .prompt([
+            {
+                name: "readme_description",
+                type: "input",
+                message: "Please provide the description for your readme file.",
+            },
+        ])
+        .then((res) => {
+            content.description = res.readme_description;
+            getTableContent();
+        });
+};
+
+//get the table of contents
+const getTableContent = () => {
+    inquirer
+        .prompt([
+            {
+                name: "tableOfContents",
+                type: "input",
+                message: "Please provide the table of contents text.",
+            },
+        ])
+        .then((res) => {
+            content.toc = res.tableOfContents;
+            getInstallQuestions();
+        });
+};
+
+const getInstallQuestions = () => {
+    inquirer
+        .prompt([
+            {
+                name: "installation",
+                type: "input",
+                message: "Please provide the installation instructions for your application?",
+            },
+        ])
+        .then((res) => {
+            content.installation = res.installation;
+            getUsage();
+        });
+};
+
+const getUsage = () => {
+    inquirer
+        .prompt([
+            {
+                name: "howToUse",
+                type: "input",
+                message: "Please provide a description of how to use your application",
+            },
+        ])
+        .then((res) => {
+            content.usage = res.howToUse;
+            getLicense();
+        });
+};
+
+const getLicense = () => {
+    inquirer
+        .prompt([
+            {
+                name: "license",
+                type: "list",
+                message: "What type of license do you wish to utilize with your application",
+                choices: [
+                    "MIT License",
+                    "GNU GPLv3",
+                ]
+            },
+        ])
+        .then((res) => {
+            content.license = res.license;
+            getContributing();
+        });
+};
+
+const getContributing = () => {
+    inquirer
+        .prompt([
+            {
+                name: "contribute",
+                type: "list",
+                message: "Would you like to allow other developers to contribute to your application?",
+                choices: [
+                    "Yes",
+                    "No",
+                ]
+            },
+        ])
+        .then((res) => {
+            content.contributing = res.contribute;
+            getTests();
+        });
+};
+
+const getTests = () => {
+    inquirer
+        .prompt([
+            {
+                name: "howToUse",
+                type: "input",
+                message: "What tests can your users run to ensure your application is running correctly?",
+            },
+        ])
+        .then((res) => {
+            content.tests = res.howToUse;
+            getQuestions();
+        });
+};
+
+const getQuestions = () => {
+    inquirer
+        .prompt([
+            {
+                name: "questions",
+                type: "checkbox",
+                message: "Which methods can users use to contact you with questions?",
+                choices: [
+                    "email",
+                    "phone",
+                    "github",
+                    "linkedin",
+                    "postal mail",
+                ]
+            },
+        ])
+        .then((res) => {
+            content.questions = res.questions;
+            console.log(content);
+        });
+};
+startApp();
+
+
+
+/*
+inquirer.prompt([
+    //TODO: Title question
+    {
+        type: "input",
+        message: "What would you like the title of your document to be?",
+        name: "title",
+    },
+
+    //TODO: Description question
+    {
+        type: "input",
+        message: "Please provide the description for your readme file.",
+        name: "readmeDescription",
+    },
+    //TODO: Table of Contents Question
+    {
+        type: "input",
+        message: "Please provide the table of contents text.",
+        name: "tableOfContents",
+    },
+    //TODO: Installation Question
+    {
+        type: "input",
+        message: "Please provide the installation instructions for your application?",
+        name: "installation",
+    },
+    //TODO: Usage Question
+    {
+        type: "input",
+        message: "Please provide a description of how to use your application",
+        name: "howToUse",
+    },
+    //TODO: License question
+    {
+        type: "list",
+        message: "What type of license do you wish to utilize with your application",
+        name: "license",
+        choices: [
+            "MIT License",
+            "GNU GPLv3",
+        ]
+    },
+    //TODO: Contributing question
+    {
+        type: "list",
+        message: "Would you like to allow other developers to contribute to your application?",
+        name: "contribute",
+        choices: [
+            "Yes",
+            "No",
+        ]
+    },
+    //TODO: Tests question
+    {
+        type: "input",
+        message: "What tests can your users run to ensure your application is running correctly?",
+        name: "howToUse",
+    },
+
+    //TODO: Questions
+    {
+        type: "checkbox",
+        message: "Which methods can users use to contact you with questions?",
+        name: "questions",
+        choices: [
+            "email",
+            "phone",
+            "github",
+            "linkedin",
+            "postal mail",
+        ]
+    }
+
+    //writing a readme in a markdown file
+]).then(function (data) {
+
+    var filename = data.title.toLowerCase().split(' ').join('') + ".md";
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+
+    });
+});
+*/
--- README.md
+++ README.md
@@ -1,30 +1,40 @@
 
 ![License: MIT ](https://img.shields.io/badge/License-MIT-yellow.svg)
-# Readme generator
+# Readme Generator
 
-### Authors: 
+## Authors: 
 Alex Milroy  
 
 ## Table of contents:
-[Files](#Files)
-[Description](#Description)
-[Installation](#Installation)
-[Uses](#Uses)
-[License](#License)
-[Contributions](#Contributions)
-[Tests](#Tests)
-[Contact](#Contact)
+* [Files](#Files)
+* [Description](#Description)
+* [Installation](#Installation)
+* [Uses](#Uses)
+* [License](#License)
+* [Contributions](#Contributions)
+* [Tests](#Tests)
+* [Contact](#Contact)
 
 ## Files
+* index.js
+* package-lock.json
+* package.json
+* readme.md
+* .gitignore
+* how to add more licenses.png
+* initilization.png
+* output.png
 
 ## Description
-This is a readme generator application that helps users automatically create readme files
+This is a readme genereator application that helps users automatically create readme files. It uses a module called prompt that is utilized to ask the user a number of questions about the information they with to include in their readme file. Once the user answers all the questions, their input is automatically inserted into a readme file with existing markup. This saves the user time by not having to enter any markup to style their readme, they can simply provide the content for each section through the terminal prompts.
+
+A video walkthrough of the program is available to view at : https://drive.google.com/file/d/1O3TSN2eVNroN0a699o3KRFn9RLWO2dSF/view
 
 ## Installation
-There are no isntallation instructions
+Copy all files into a working directory, and open an integrated terminal window. Once open, ensure the terminal has the correct folder open. Type "node index.js" into the terminal window, and if you are prompted for answers the program has been installed correctly.
 
 ## Uses
-To use the application, simply run the command "node index.js"
+Use this application to more quickly and easily build readme files for any project or application you have created.
 
 ## License
 This application is goverened by the MIT license.
@@ -33,8 +43,13 @@ This application is goverened by the MIT license.
 Yes
 
 ## Tests
-They can run the application
+run node index.js and follow the prompts. Once complete, check the readme files to ensure it was written correctly
     
 ## Contact:
-<github.com/ajm5099>
-<github.com/amilroy@gmail.com>
+github.com/ajm5099
+github.com/amilroy@gmail.com
+
+## Images:
+![Site Screenshot](images/how_to_add_more_licenses.png)
+![Site Screenshot](images/initilization.png)
+![Site Screenshot](images/output.png)
